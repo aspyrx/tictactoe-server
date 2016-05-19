36d49e5592b0a1e089808e24c395b2ee7e913b93
Stan Zhang <stan.zhang2@gmail.com>
2016-05-19 02:13:46 -0400

    More gameplay logic

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const EventEmitter = require('events');
 const path = require('path');
 const flags = require('flags');
 const express = require('express');
@@ -20,53 +21,72 @@ class Board {
 
     isValid(x, y) {
         return 0 <= y && y < this.values.length
-            && 0 <= x && x < this.values[y].length;
+            && 0 <= x && x < this.values[y].length
+            && this.values[y][x] === -1;
     }
 
     move(x, y, val) {
         if (this.isValid(x, y)) {
             this.values[y][x] = val;
+            return true;
         }
+
+        return false;
     }
 }
 
 
-class Game {
-    constructor(p0, ...ps) {
-        const id = p0.id;
-        const p = [p0, ...ps];
+class Game extends EventEmitter {
+    broadcast(event, data) {
+        io.of('/tictactoe').to(this.id).emit(event, data);
+    }
+
+    constructor(p0, ...others) {
+        super();
+
+        this.id = p0.id;
+
+        const p = [p0, ...others];
         const board = new Board();
 
         let turn = -1;
 
         // Make everyone else join the same room as p0
-        ps.map(pl => pl.join(id));
+        others.map(pl => pl.join(this.id));
 
-        function broadcast(event, data) {
-            io.of('/tictactoe').to(id).emit(event, data);
-        }
-
-        function nextTurn() {
-            broadcast('board', board.values);
+        const nextTurn = () => {
+            this.broadcast('board', board.values);
             turn++;
             turn %= p.length;
             const pl = p[turn];
+            const moveHandler = (x, y) => {
+                if (board.move(x, y, turn)) {
+                    nextTurn();
+                } else {
+                    pl.once('move', moveHandler);
+                }
+            }
             pl.once('move', moveHandler);
             pl.emit('your turn');
         }
 
-        const moveHandler = (x, y) => {
-            board.move(x, y, turn);
-            nextTurn();
-        };
+        // End the game when a player disconnects
+        const endGame = () => {
+            p.map(pl => pl.emit('game end')
+                   .removeAllListeners('move')
+                   .removeAllListeners('disconnect'));
+            super.emit('game end');
+        }
 
-        broadcast('game start')
+        p.map(pl => pl.on('disconnect', endGame));
+
+        this.broadcast('game start')
         nextTurn();
     }
 }
 
 let waiting = null;
-const games = []
+const games = {}
 
 io.of('/tictactoe').on('connection', p0 => {
     const disconnectHandler = () => waiting = null;
@@ -78,7 +98,9 @@ io.of('/tictactoe').on('connection', p0 => {
         p0.removeListener('disconnect', disconnectHandler);
         const p1 = waiting;
         waiting = null;
-        games.push(new Game(p0, p1));
+        var game = new Game(p0, p1);
+        game.on('game end', () => delete games[game.id]);
+        games[game.id] = game;
     }
 });
 
